fd46514209a8f285570715d8d5fade203479e478
Poornimagh <poornima@appinessworld.com>
2023-02-28 19:32:41 +0530

    ui fixes

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -13,7 +13,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Welcome onboard!'),
+      child: Center(
+          child: Text(
+        'Welcome onboard!',
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          fontSize: 18.0,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
     );
   }
 }
